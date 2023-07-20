784fce7cfd88dcb77c7423225ca55656cbaeb7be
Abdul Rauof Nasery <rauf.nasery143@gmail.com>
2023-07-20 08:07:56 -0600

    nothing works

--- FinalProject.client/src/components/VaultCard.vue
+++ FinalProject.client/src/components/VaultCard.vue
@@ -3,12 +3,16 @@
     <!-- <div class="row"> -->
     <section>
         <div class="col-md-3 col-12 img-fluid">
-            <!-- <router-link :to="{ name: 'KeepInVault', params: { vaultId: myVault.id } }"> -->
-            <img @click="$router.push({ name: 'KeepInVault', params: { vaultId: myVault.id } })"
-                class="m-2 rounded elevation-4" :src="myVault?.img" alt="">
-            <p>{{ myVault.name }}</p>
-            <!-- </router-link> -->
-            <!-- <p v-if="myVault?.isPrivate" class="text-danger">THIS VAULT IS NOT OPEN TO PUBLIC</p> -->
+            <router-link :to="{ name: 'KeepInVault', params: { vaultId: myVault.id } }">
+                <img class="m-2 rounded elevation-4" :src="myVault?.img" alt="">
+                <p>{{ myVault.name }}</p>
+                <img v-if="myVault.isPrivate" @click="$router.push({ name: 'Home', params: '/' })"
+                    src="https://images.unsplash.com/photo-1572670014853-1d3a3f22b40f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c3RvcHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                    alt="">
+
+            </router-link>
+            <!-- if router link does not work put use this instead @click="$router.push({ name: 'KeepInVault', params: { vaultId: myVault.id } })" -->
+
             <p v-show="account.id == myVault.creatorId"><i class="mdi mdi-delete text-danger btn"
                     @click="deleteVault(myVault.id)"></i></p>
         </div>
